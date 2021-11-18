ea0f3a8c3b14979adc64c3df1e68665024701486
Zane Russell <zane@zanes-mbp.lan>
2021-11-18 11:20:25 -0700

    fix footer links

--- src/pages/MainPage/Footer/Footer.jsx
+++ src/pages/MainPage/Footer/Footer.jsx
@@ -5,19 +5,36 @@ import styles from './footer.module.scss'
 const Footer = () => {
   return (
     <div className={styles.wrapper}>
-      <a href="https://twitter.com/litprotocol" target="_blank">
+      <a
+        className={styles.linkTwitter}
+        href="https://twitter.com/litprotocol"
+        target="_blank"
+      >
         <Icons.Twitter />
       </a>
 
-      <a href="https://litgateway.com/discord" target="_blank">
+      <a
+        className={styles.linkDiscord}
+        href="https://litgateway.com/discord"
+        target="_blank"
+      >
         <Icons.Discord />
       </a>
 
-      <a href="https://blog.litprotocol.com/">Blog</a>
+      <a className={styles.linkBlog} href="https://blog.litprotocol.com/">
+        Blog
+      </a>
 
-      <a href="https://developer.litprotocol.com/">Developer Docs</a>
+      <a
+        className={styles.linkDeveloper}
+        href="https://developer.litprotocol.com/"
+      >
+        Developer Docs
+      </a>
 
-      <a href="#">Contact</a>
+      <a className={styles.linkContact} href="#">
+        Contact
+      </a>
     </div>
   )
 }
