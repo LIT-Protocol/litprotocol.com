bea42c99a3935594605a876005d3e4e1fb33e8b0
Zane Russell <zane@zanes-mbp.lan>
2021-11-20 13:19:33 -0700

    update to handle already subscribed

---
+++ src/pages/MainPage/Hero/RightHeroBlock/CustomForm.jsx
@@ -0,0 +1,75 @@
+import styles from './right-hero-block.module.scss'
+import { Button } from '../../../../components'
+import cx from 'classnames'
+import { useEffect, useState } from 'react'
+
+const CustomForm = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('')
+  const [inputMessageClass, setInputMessageClass] = useState('error')
+  const [inputMessage, setInputMessage] = useState('')
+  const [added, setAdded] = useState(false)
+
+  useEffect(() => {
+    if (message && message.includes('Thank you')) {
+      setInputMessageClass('success')
+      setInputMessage(message)
+    } else if (message && message.includes('already subscribed')) {
+      setInputMessageClass('success')
+      setInputMessage('This email is already subscribed!')
+    }
+  }, [message])
+
+  const validateEmail = (email) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+  }
+
+  const submit = async () => {
+    if (!validateEmail(email)) {
+      setInputMessageClass('error')
+      setInputMessage('Please enter a valid email')
+      return false
+    }
+
+    if (!added) {
+      setInputMessage(null)
+      setAdded(true)
+    }
+
+    setAdded(true)
+
+    onValidated({
+      EMAIL: email,
+    })
+  }
+
+  const transferEmailValue = (e) => {
+    e.preventDefault()
+    setEmail(e.currentTarget.value)
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.inputContainer}>
+        <input
+          className={cx(styles.input, styles['primary'], styles.emailInput)}
+          type="text"
+          value={email}
+          onChange={transferEmailValue}
+          placeholder="Email address"
+        />
+        <span className={styles[inputMessageClass]}>{inputMessage}</span>
+      </span>
+      {!added ? (
+        <Button className={styles.getUpdatesButton} onClick={submit}>
+          Get Updates
+        </Button>
+      ) : (
+        <Button className={styles.addedForUpdates}>Added!</Button>
+      )}
+    </div>
+  )
+}
+
+export default CustomForm
--- src/pages/MainPage/Hero/RightHeroBlock/RightHeroBlock.jsx
+++ src/pages/MainPage/Hero/RightHeroBlock/RightHeroBlock.jsx
@@ -1,70 +1,12 @@
 import { useState } from 'react'
-import { Button } from 'components'
-
-import styles from './right-hero-block.module.scss'
 import MailchimpSubscribe from './MailchimpSubscribe'
-import cx from 'classnames'
+import CustomForm from './CustomForm'
 
 const RightHeroBlock = () => {
-  const validateEmail = (email) => {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return re.test(String(email).toLowerCase())
-  }
+  const [inputMessage, setInputMessage] = useState('')
 
   const addUrl =
     'https://litprotocol.us20.list-manage.com/subscribe/post?u=37eeb6bbc8f1fee01838fd9df&id=4f37de62dc'
-
-  const CustomForm = ({ status, message, onValidated }) => {
-    const [email, setEmail] = useState('')
-    const [inputMessage, setInputMessage] = useState('')
-    const [added, setAdded] = useState(false)
-
-    const submit = async () => {
-      if (!validateEmail(email)) {
-        setInputMessage('Please enter a valid email')
-        return false
-      }
-
-      if (!added) {
-        setInputMessage(null)
-        setAdded(true)
-      }
-
-      setAdded(true)
-
-      onValidated({
-        EMAIL: email,
-      })
-    }
-
-    const transferEmailValue = (e) => {
-      e.preventDefault()
-      setEmail(e.currentTarget.value)
-    }
-
-    return (
-      <div className={styles.wrapper}>
-        <span className={styles.inputContainer}>
-          <input
-            className={cx(styles.input, styles['primary'], styles.emailInput)}
-            type="text"
-            value={email}
-            onChange={transferEmailValue}
-            placeholder="Email address"
-          />
-          <span className={styles.error}>{inputMessage}</span>
-        </span>
-        {!added ? (
-          <Button className={styles.getUpdatesButton} onClick={submit}>
-            Get Updates
-          </Button>
-        ) : (
-          <Button className={styles.addedForUpdates}>Added!</Button>
-        )}
-      </div>
-    )
-  }
 
   return (
     <div>
