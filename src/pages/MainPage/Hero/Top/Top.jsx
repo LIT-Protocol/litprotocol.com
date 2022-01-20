7ae8f2ec0772c15d0f65a6dcfa2c2db0c34e4b70
zanedr <zanedanielrussell@gmail.com>
2022-01-20 16:44:22 -0700

    add hamburger menu for mobile

--- src/pages/MainPage/Hero/Top/Top.jsx
+++ src/pages/MainPage/Hero/Top/Top.jsx
@@ -1,5 +1,7 @@
 import styles from './top.module.scss'
 import { useState } from 'react'
+import exitIcon from '../assets/exitIcon.svg'
+import hamburgerIcon from '../assets/hamburgerIcon.svg'
 
 const Top = () => {
   const [hamburgerMenuOpen, setHamburgerMenuOpen] = useState(false)
@@ -35,44 +37,55 @@ const Top = () => {
             Contact
           </a>
         </span>
-        {/*<nav className={styles.hamburgerMenuContainer}>*/}
-        {/*  <button onClick={() => setHamburgerMenuOpen(!hamburgerMenuOpen)}>Menu</button>*/}
-        {/*  {!!hamburgerMenuOpen && (*/}
-        {/*    <ul className={styles.hamburgerMenu}>*/}
-        {/*      <li>*/}
-        {/*        <a className={styles.linkBlog} href={`${window.location.href}about`}>*/}
-        {/*          About*/}
-        {/*        </a>*/}
-        {/*      </li>*/}
-        {/*      <li>*/}
-        {/*        <a className={styles.linkBlog} href='https://blog.litprotocol.com/'>*/}
-        {/*          Blog*/}
-        {/*        </a>*/}
-        {/*      </li>*/}
-        {/*      <li>*/}
-        {/*        <a href='https://litgateway.com'>Gateway</a>*/}
-        {/*      </li>*/}
+        <nav className={styles.hamburgerMenuContainer}>
+          <button
+            className={styles.menuButton}
+            onClick={() => setHamburgerMenuOpen(!hamburgerMenuOpen)}
+          >
+            <img src={hamburgerMenuOpen ? exitIcon : hamburgerIcon} />
+          </button>
+          {!!hamburgerMenuOpen && (
+            <ul className={styles.hamburgerMenu}>
+              <li className={styles.menuLink}>
+                <a
+                  className={styles.linkBlog}
+                  href={`${window.location.href}about`}
+                >
+                  About
+                </a>
+              </li>
+              <li className={styles.menuLink}>
+                <a
+                  className={styles.linkBlog}
+                  href="https://blog.litprotocol.com/"
+                >
+                  Blog
+                </a>
+              </li>
+              <li className={styles.menuLink}>
+                <a href="https://litgateway.com">Gateway</a>
+              </li>
 
-        {/*      <li>*/}
-        {/*        <a*/}
-        {/*          className={styles.linkDeveloper}*/}
-        {/*          href='https://developer.litprotocol.com/docs/intro'*/}
-        {/*        >*/}
-        {/*          Docs*/}
-        {/*        </a>*/}
-        {/*      </li>*/}
+              <li className={styles.menuLink}>
+                <a
+                  className={styles.linkDeveloper}
+                  href="https://developer.litprotocol.com/docs/intro"
+                >
+                  Docs
+                </a>
+              </li>
 
-        {/*      <li>*/}
-        {/*        <a*/}
-        {/*          className={styles.linkContact}*/}
-        {/*          href='https://airtable.com/shr2NWJbH1Y6Y3kOU'*/}
-        {/*        >*/}
-        {/*          Contact*/}
-        {/*        </a>*/}
-        {/*      </li>*/}
-        {/*    </ul>*/}
-        {/*  )}*/}
-        {/*</nav>*/}
+              <li className={styles.menuLink}>
+                <a
+                  className={styles.linkContact}
+                  href="https://airtable.com/shr2NWJbH1Y6Y3kOU"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          )}
+        </nav>
       </div>
     </div>
   )
