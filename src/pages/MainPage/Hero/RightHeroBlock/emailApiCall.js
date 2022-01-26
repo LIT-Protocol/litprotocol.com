c8f6df55be46cebaf21056a9dbaed5740ef750ff
zanedr <zanedanielrussell@gmail.com>
2022-01-26 14:58:35 -0700

    update email call

--- src/pages/MainPage/Hero/RightHeroBlock/emailApiCall.js
+++ src/pages/MainPage/Hero/RightHeroBlock/emailApiCall.js
@@ -1,20 +1,10 @@
-const SibApiV3Sdk = require('sib-api-v3-typescript')
-require('dotenv').config()
-
-const emailApi = process.env.REACT_APP_SEND_IN_BLUE_API
-
 export const postUser = (body) => {
-  console.log('CHECK PROCESS', process.env)
-  let apiInstance = new SibApiV3Sdk.ContactsApi()
-
-  let apiKey = apiInstance.authentications['apiKey']
-
-  apiKey.apiKey = emailApi
-
-  let createContact = new SibApiV3Sdk.CreateContact()
-
-  createContact.email = body.email
-  createContact.listIds = [2]
-
-  return apiInstance.createContact(createContact)
+  return fetch('https://backend-dev.litgateway.com/saveSubscribe', {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  }).then((response) => response.json())
 }
