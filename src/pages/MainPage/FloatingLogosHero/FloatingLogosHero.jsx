d221061e9bf8f1f0c767a6e8e972ba1c1c5cbfec
zanedr <zanedanielrussell@gmail.com>
2022-01-27 13:13:51 -0700

    cloudflare logo added + responsive for tablet and mobile

--- src/pages/MainPage/FloatingLogosHero/FloatingLogosHero.jsx
+++ src/pages/MainPage/FloatingLogosHero/FloatingLogosHero.jsx
@@ -4,17 +4,36 @@ const FloatingLogosHero = () => {
   return (
     <span>
       <span className={styles.serviceLogo} />
-      <a href={'https://litgateway.com/apps/zoom'} target="_blank">
+      <a
+        href={'https://litgateway.com/apps/zoom'}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div className={styles.zoomLogo} />
       </a>
-      <a href={'https://litgateway.com/apps'} target="_blank">
+      <a href={'https://litgateway.com/apps'} target="_blank" rel="noreferrer">
         <div className={styles.ifpsLogo} />
       </a>
-      <a href={'https://litgateway.com/apps/google-drive'} target="_blank">
+      <a
+        href={'https://litgateway.com/apps/google-drive'}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div className={styles.googleLogo} />
       </a>
-      <a href={'https://litgateway.com/apps/gather-town'} target="_blank">
+      <a
+        href={'https://litgateway.com/apps/gather-town'}
+        target="_blank"
+        rel="noreferrer"
+      >
         <div className={styles.gatherLogo} />
+      </a>
+      <a
+        href={'https://litgateway.com/apps/cloudflare'}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div className={styles.cloudflareLogo} />
       </a>
     </span>
   )
